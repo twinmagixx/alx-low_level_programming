b73708e305f7babd0ceac2834814908e4b24cd9e
twinmagixx <andilendwandwe1902@gmail.com>
2023-07-07 09:27:04 +0200

    arg commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Print program name
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
